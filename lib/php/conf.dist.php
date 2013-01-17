1c329bca947859c9aa5a00a9e626b0a92297623a
Goodchips <ahbouju@gmail.com>
2013-01-17 23:44:51 +0100

    Ajout fichier de configuration

---
+++ lib/php/conf.dist.php
@@ -0,0 +1,8 @@
+<?php 
+	define('__ROOT_URL','http://'. $_SERVER['HTTP_HOST'] .'/'.'keyri/');
+	
+	define('DB_HOST', 'localhost');
+	define('DB_USER', '');
+	define('DB_PASSWORD', '');
+	define('DB_NAME', 'keyri');
+?>
