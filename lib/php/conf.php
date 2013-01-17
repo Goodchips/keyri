7f27c415777af164d71f7b39e276d290b69409d5
Goodchips <ahbouju@gmail.com>
2013-01-17 23:43:20 +0100

    Suppression fichier de conf

--- lib/php/conf.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	define('__ROOT_URL','http://'. $_SERVER['HTTP_HOST'] .'/'.'keyri/');
-	
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_NAME', 'keyri');
-?>
